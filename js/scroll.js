3c2d797221e375006a2f9eb36cfdd8e2f52ae762
KauaArruda <117871994+KauaArruda@users.noreply.github.com>
2022-11-16 21:44:34 -0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+ScrollReveal({
+    reset: true
+});
+
+
+ScrollReveal().reveal('.sobre', {
+    duration: 3000
+});
+
+
+ScrollReveal().reveal('.portfolio', {
+    duration: 3000
+});
+
+
+ScrollReveal().reveal('.contato', {
+    duration: 3000
+});
+
+
